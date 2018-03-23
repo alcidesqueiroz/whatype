b87963b82a6cf06bd6fd0876cecd1a0ef6f24a03
Alcides Queiroz <alcidesqueiroz@gmail.com>
2018-03-23 00:17:01 -0300

    module implementation

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
 
-function whatype() {}
+function whatype(val) {
+  const typeOf = typeof val;
+
+  if (['function', 'undefined', 'string', 'boolean'].indexOf(typeOf) > -1) return typeOf;
+  if (val === null) return 'null';
+  if (val.constructor === Array) return 'array';
+  if (val === Object(val)) return 'object';
+  if (isNaN(val)) return 'not-a-number';
+  if (!isNaN(parseFloat(val)) && isFinite(val)) return 'number';
+  if (val === Infinity) return 'infinity';
+  if (val === -Infinity) return '-infinity';
+}
 
 module.exports = whatype;
